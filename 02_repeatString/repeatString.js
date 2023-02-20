053d9d00f86e0390e783a922dbe3e9192611cfc4
Yengera99 <ryengera10@gmail.com>
2023-02-20 22:15:37 +0530

    Completed first 3 excercises with all test cases passing

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    
+    let concatString = '';
+    if(num<0){
+        concatString='ERROR';
+        return concatString;
+    }
+    for(let i=0; i<num; i++){
+        concatString = concatString.concat(string);
+        
+    }
+    return concatString;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const arrStr = string.split("");
+    let modStr = '';
+    for(let i=arrStr.length-1; i>=0; i--){
+        modStr= modStr.concat(arrStr[i]);
+    }
+    return modStr;
 };
 
 // Do not edit below this line
