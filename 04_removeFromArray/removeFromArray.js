8a8fd4a358aa22d5d9f00236adcb9c2b87e3f1c6
Yengera99 <ryengera10@gmail.com>
2023-02-20 23:08:32 +0530

    Completed Excercise no.4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...args) {
+    const newArr = [];
+    arr.forEach((elem) => {
+        if(!args.includes(elem)){
+            newArr.push(elem);
+        }
+    })
+    return newArr;
 };
 
 // Do not edit below this line
